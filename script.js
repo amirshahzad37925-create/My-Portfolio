dc0fb7cb22e7cf2b3fc84ce03b5c4c51549b3d02
amirshahzad37925-create <amirshahzad37925@gmail.com>
2025-09-01 22:24:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,27 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+// Typing Text Effect
+const texts = ["Lead Generation Specialist", "Web Research Expert", "Freelancer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing-text").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1500); // ruk kar next text start hoga
+  } else {
+    setTimeout(type, 100);
+  }
+})();
