1a98080f17ca7fd0ec546df8606452f3c0d85d5a
amirshahzad37925-create <amirshahzad37925@gmail.com>
2025-09-19 11:31:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.2 }); // 20% visible hone par animation start hogi
+
+document.querySelectorAll('.animate').forEach(el => observer.observe(el));
 
