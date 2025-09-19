254c6b63a5e3047dd22c4ebe780aa2694541dc2b
amirshahzad37925-create <amirshahzad37925@gmail.com>
2025-09-19 11:56:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ const observer = new IntersectionObserver((entries) => {
 
 document.querySelectorAll('.animate').forEach(el => observer.observe(el));
 
+// Counter animation
+const counters = document.querySelectorAll(".counter");
+const speed = 200; // Speed adjust karne ke liye
+
+counters.forEach(counter => {
+  const animate = () => {
+    const value = +counter.getAttribute("data-target");
+    const data = +counter.innerText;
+
+    const increment = value / speed;
+
+    if (data < value) {
+      counter.innerText = Math.ceil(data + increment);
+      setTimeout(animate, 20);
+    } else {
+      counter.innerText = value;
+    }
+  };
+
+  // Trigger only when section is visible
+  let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        animate();
+        observer.unobserve(counter); // run once only
+      }
+    });
+  }, { threshold: 0.6 });
+
+  observer.observe(counter);
+});
